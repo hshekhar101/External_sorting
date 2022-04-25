04db50d46659a8d8678dd4b2d59ab8dc03e90e54
hiitgmanshu <83654752+hiitgmanshu@users.noreply.github.com>
2022-04-25 12:13:28 +0530

    update_practice.cpp

--- Practice.cpp
+++ Practice.cpp
@@ -1,90 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<vector<int>> v;
-int b, Record_per_page, records;
+int x;
+int records_per_page;  
+int records;
 void first_iter()
 {
-   for (int i = 0; i < v.size(); i += b)
-   {
-      for (int j = i; j < min(i + b, (int)v.size()); j++)
-         sort(v[j].begin(), v[j].end());
-   }
+    for (int i = 0; i < v.size(); i += x)
+    {
+        for (int j = i; j < min(i + x, (int)v.size()); j++)
+            sort(v[j].begin(), v[j].end());
+    }
 }
-void external_sort()
+void ext_sort()
 {
-   while (v.size() > 1)
-   {
-      vector<vector<int>> v2;
+    while (v.size() > 1)
+    {
+        vector<vector<int>> v2;
 
-      for (int i = 0; i < v.size(); i += b - 1)
-      {
-         int brought_pages;
-         if (v.size() - i >= b - 1)
-         {
-            brought_pages = b - 1;
-         }
-         else
-            brought_pages = v.size() - i;
-         int arr[brought_pages];
-         for (int j = 0; j < brought_pages; j++)
-         {
-            arr[j] = 0;
-         }
-         vector<vector<int>> temp1;
-
-         vector<int> temp;
-         while (true)
-         {
-            int mini = 10000000;
-            int ind = -1;
-            for (int j = i; j < i + brought_pages; j++)
+        for (int i = 0; i < v.size(); i += x - 1)
+        {
+            int number_of_pages_brought;
+            if (v.size() - i >= x - 1)
             {
-               if (arr[j - i] < v[j].size())
-               {
-                  if (v[j][arr[j - i]] < mini)
-                  {
-                     mini = min(v[j][arr[j - i]], mini);
-                     ind = j - i;
-                     //                            cout<<mini<<" ";
-                  }
-               }
+                number_of_pages_brought = x - 1;
             }
-            //                cout<<endl;
-            if (ind == -1)
-               break;
-            arr[ind]++;
-            temp.push_back(mini);
-         }
-         v2.push_back(temp);
-      }
-      v = v2;
-   }
+            else
+                number_of_pages_brought = v.size() - i;
+            int arr[number_of_pages_brought];
+            for (int j = 0; j < number_of_pages_brought; j++)
+            {
+                arr[j] = 0;
+            }
+            vector<vector<int>> temp1;
+
+            vector<int> temp;
+            while (true)
+            {
+                int mini = 10000000;
+                int ind = -1;
+                for (int j = i; j < i + number_of_pages_brought; j++)
+                {
+                    if (arr[j - i] < v[j].size())
+                    {
+                        if (v[j][arr[j - i]] < mini)
+                        {
+                            mini = min(v[j][arr[j - i]], mini);
+                            ind = j - i;
+                            //                            cout<<mini<<" ";
+                        }
+                    }
+                }
+                //                cout<<endl;
+                if (ind == -1)
+                    break;
+                arr[ind]++;
+                temp.push_back(mini);
+            }
+            v2.push_back(temp);
+        }
+        v = v2;
+    }
 }
 int main()
 {
-   cin >> b >> Record_per_page >> records;
-   int i = 0;
-   vector<int> temp;
-   //    cout<<records<<endl;
-   while (i < records)
-   {
-      int x;
-      cin >> x;
-      temp.push_back(x);
-      i++;
-      if (temp.size() == Record_per_page)
-      {
-         v.push_back(temp);
-         temp.clear();
-      }
-   }
-   if (temp.size() > 0)
-      v.push_back(temp);
-   first_iter();
-   external_sort();
-   for (auto x : v)
-   {
-      for (auto y : x)
-         cout << y << endl;
-   }
+    cin >> x >> records_per_page >> records;
+    int i = 0;
+    vector<int> temp;
+    //    cout<<records<<endl;
+    while (i < records)
+    {
+        int x;
+        cin >> x;
+        temp.push_back(x);
+        i++;
+        if (temp.size() == records_per_page)
+        {
+            v.push_back(temp);
+            temp.clear();
+        }
+    }
+    if (temp.size() > 0)
+        v.push_back(temp);
+    first_iter();
+    ext_sort();
+    for (auto x : v)
+    {
+        for (auto y : x)
+            cout << y << endl;
+    }
 }
